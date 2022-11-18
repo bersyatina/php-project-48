47e341963602df0ddfa35f9348666adf7f4da089
Apanasenko Andrey <aw_apanasenko@mail.ru>
2022-11-18 12:17:23 +0000

    fix Differ.php

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -11,16 +11,43 @@ function getComparison($value, $first = [], $second = []): array
     if (array_key_exists($value, $first) && array_key_exists($value, $second)) {
         if (!is_array($first[$value]) || !is_array($second[$value])) {
             if ($first[$value] === $second[$value]) {
-                return [TARGET, ' ', $value, $first[$value]];
+                return [
+                    'operator' => ' ',
+                    'key' => $value,
+                    'value' => $first[$value]
+                ];
             } else {
-                return [[TARGET, '-', $value, $first[$value]], [TARGET, '+', $value, $second[$value]]];
+                return [
+                    [
+                        'operator' => '-',
+                        'key' => $value,
+                        'value' => $first[$value]
+                    ],
+                    [
+                        'operator' => '+',
+                        'key' => $value,
+                        'value' => $second[$value]
+                    ]
+                ];
             }
         }
-        return [TARGET, ' ', $value, $first[$value]];
+        return [
+            'operator' => ' ',
+            'key' => $value,
+            'value' => $first[$value]
+        ];
     } elseif (array_key_exists($value, $first) && !array_key_exists($value, $second)) {
-        return [TARGET, '-', $value, $first[$value]];
+        return [
+            'operator' => '-',
+            'key' => $value,
+            'value' => $first[$value]
+        ];
     } elseif (!array_key_exists($value, $first) && array_key_exists($value, $second)) {
-        return [TARGET, '+', $value, $second[$value]];
+        return [
+            'operator' => '+',
+            'key' => $value,
+            'value' => $second[$value]
+        ];
     }
     return [];
 }
@@ -29,17 +56,14 @@ function getResultToString(array $fileArr): string
 {
     $res = '';
     foreach ($fileArr as $target) {
-        if (is_array($target)){
+        if (is_array($target)) {
             if (in_array(TARGET, $target)) {
-                dump("Вывод", $target[2], $target[3]);
                 array_shift($target);
                 return getResultToString($target);
             } else {
                 return getResultToString($target);
             }
         }
-//        array_shift($target);
-//        dump("ПОСЛЕ", $target);
         $res .= json_encode($target);
     }
 
@@ -56,15 +80,15 @@ function generateKeys(array $firstArr, array $secondArr): array
 
 function getResultToArray(array $filesKeys, array $firstFileArr, array $secondFileArr)
 {
-    $result =array_reduce ($filesKeys, function ($acc, $value) use ($firstFileArr, $secondFileArr) {
+    $result = array_reduce($filesKeys, function ($acc, $value) use ($firstFileArr, $secondFileArr) {
         if (array_key_exists($value, $firstFileArr) && array_key_exists($value, $secondFileArr)) {
             if (is_array($firstFileArr[$value]) && is_array($secondFileArr[$value])) {
                 $arrayKeys = generateKeys($firstFileArr[$value], $secondFileArr[$value]);
 
                 $acc[$value] = getComparison($value, $firstFileArr, $secondFileArr);
-                $acc[$value][3] = getResultToArray($arrayKeys, $firstFileArr[$value], $secondFileArr[$value]);
-                return $acc
-                    ;            }
+                $acc[$value]['value'] = getResultToArray($arrayKeys, $firstFileArr[$value], $secondFileArr[$value]);
+                return $acc;
+            }
         }
         $res1 = getComparison($value, $firstFileArr, $secondFileArr);
 
@@ -77,17 +101,6 @@ function getResultToArray(array $filesKeys, array $firstFileArr, array $secondFi
         return $acc;
     }, []);
     return array_values($result);
-//    $res = "";
-//    foreach ($result as $item) {
-//        $res .= "  " . $item[0] . " " . str_replace('"', "", json_encode([$item[1] => $item[2]]));
-//    }
-//    $res .= "";
-//
-//    $res = str_replace("}", "\n", $res);
-//    $res = str_replace("{", "", $res);
-//    $res = str_replace(":", ": ", $res);
-//
-//    return "{\n" . $res . "}";
 }
 
 
@@ -99,7 +112,7 @@ function genDiff(string $firstFile, string $secondFile, string $format = 'json')
     $secondFileArr = decode($pathToFiles . $secondFile);
     $filesKeys = generateKeys($firstFileArr, $secondFileArr);
 
-    $arrResult = getResultToArray($filesKeys, $firstFileArr, $secondFileArr);
+    $arrResult = dd(getResultToArray($filesKeys, $firstFileArr, $secondFileArr));
 
     return getResultToString($arrResult);
 }
