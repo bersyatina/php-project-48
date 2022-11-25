e001e4d779eb734e4f440e2dabfea1bb6ddd83b5
Apanasenko Andrey <aw_apanasenko@mail.ru>
2022-11-25 14:03:28 +0000

    change getResultToPlain function in Formatters\Plain.php

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -9,24 +9,34 @@ function getResultToPlain(array $array, string $path = ''): string
 {
 //    dump($path, $array);
     $result = array_map(function ($item) use (&$result, $array, $path) {
+        $path = ltrim($path, ".");
         if (getDataStatus($item)){
-            $res = "'{$item['key']}' ";
+            $res = "'{$path}.{$item['key']}' ";
+            $filter = array_filter($array, fn($value) => $item['key'] === $value['key']);
             if ($item['operator'] === "+") {
-                $res .= "was added with value: ";
+                if (count($filter) !== 2) {
+                    $res .= "was added with value: ";
+                } else {
+                    $res .= "was updated. From ";
+                }
                 if (is_array($item['value'])) {
                     $res .= "[complex value]";
                 } else {
-                    $res .= $item['value'];
+                    $res .= toString($item['value']);
                 }
+
             } elseif ($item['operator'] === "-") {
                 $res .= "was removed";
             } elseif ($item['operator'] === " ") {
-                $res = "";
+                if (is_array($item['value'])) {
+//                    dump($item['key']);
+
+                    $res = getResultToPlain($item['value'], $path . "." . $item['key']);
+                }
             }
             return $res;
         }
-
     }, $array);
 
-    return "Property " . implode("\nProperty ", $result);
+    return implode("\n", $result);
 }
