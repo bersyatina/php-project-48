09fd7010af33d4919397bae3aa4f17c90b6cc541
Apanasenko Andrey <aw_apanasenko@mail.ru>
2022-12-21 05:06:45 +0000

    add dataProvider in DifferTest.php

--- tests/Differ/DifferTest.php
+++ tests/Differ/DifferTest.php
@@ -8,46 +8,71 @@ use function Differ\Differ\genDiff as genDiff;
 
 class DifferTest extends TestCase
 {
-    private $testsData;
+//    private $testsData;
 
-    public function setUp(): void
+//    public function setUp(): void
+//    {
+//        $this->testsData = [
+//            'result2' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result2'),
+//            'result3' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result3'),
+//            'result4' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result4.json'),
+//            'diffJson' => genDiff('file3.json', 'file4.json'),
+//            'diffYaml' => genDiff('file3.yml', 'file4.yaml'),
+//            'diffJsonStylish' => genDiff('file3.json', 'file4.json', 'stylish'),
+//            'diffYamlStylish' => genDiff('file3.yml', 'file4.yaml', 'stylish'),
+//            'diffJsonPlain' => genDiff('file3.json', 'file4.json', 'plain'),
+//            'diffYamlPlain' => genDiff('file3.yml', 'file4.yaml', 'plain'),
+//            'diffJsonJson' => genDiff('file3.json', 'file4.json', 'json'),
+//            'diffYamlJson' => genDiff('file3.yml', 'file4.yaml', 'json'),
+//        ];
+//    }
+
+    /**
+     * @dataProvider differProvider
+     *
+     * @param string $file1
+     * @param string $file2
+     * @param string $format
+     * @param string $result
+     * @return void
+     */
+
+    public function testGenDiff($file1, $file2, $result, $format = 'stylish'): void
     {
-        $this->testsData = [
-            'result2' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result2'),
-            'result3' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result3'),
-            'result4' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result4.json'),
-            'diffJson' => genDiff('file3.json', 'file4.json'),
-            'diffYaml' => genDiff('file3.yml', 'file4.yaml'),
-            'diffJsonStylish' => genDiff('file3.json', 'file4.json', 'stylish'),
-            'diffYamlStylish' => genDiff('file3.yml', 'file4.yaml', 'stylish'),
-            'diffJsonPlain' => genDiff('file3.json', 'file4.json', 'plain'),
-            'diffYamlPlain' => genDiff('file3.yml', 'file4.yaml', 'plain'),
-            'diffJsonJson' => genDiff('file3.json', 'file4.json', 'json'),
-            'diffYamlJson' => genDiff('file3.yml', 'file4.yaml', 'json'),
+        $expected = file_get_contents(dirname(__DIR__, 1) . "/fixtures/{$result}");
+
+        $this->assertEquals($expected, genDiff($file1, $file2, $format));
+    }
+
+    public function differProvider()
+    {
+        return [
+            ['file3.json', 'file4.json', 'result2'],
+            ['file3.yml', 'file4.yaml', 'result2'],
+            ['file3.json', 'file4.json', 'result2', 'stylish'],
+            ['file3.yml', 'file4.yaml', 'result2', 'stylish'],
+            ['file3.json', 'file4.json', 'result3', 'plain'],
+            ['file3.yml', 'file4.yaml', 'result3', 'plain'],
+            ['file3.json', 'file4.json', 'result4.json', 'json'],
+            ['file3.yml', 'file4.yaml', 'result4.json', 'json'],
         ];
     }
 
-    public function testGenDiffDefault(): void
-    {
-        $this->assertEquals($this->testsData['result2'], $this->testsData['diffJson']);
-        $this->assertEquals($this->testsData['result2'], $this->testsData['diffYaml']);
-    }
-
-    public function testGenDiffStylish()
-    {
-        $this->assertEquals($this->testsData['result2'], $this->testsData['diffJsonStylish']);
-        $this->assertEquals($this->testsData['result2'], $this->testsData['diffYamlStylish']);
-    }
-
-    public function testGenDiffPlain()
-    {
-        $this->assertEquals($this->testsData['result3'], $this->testsData['diffJsonPlain']);
-        $this->assertEquals($this->testsData['result3'], $this->testsData['diffYamlPlain']);
-    }
-
-    public function testGenDiffJson()
-    {
-        $this->assertEquals($this->testsData['result4'], $this->testsData['diffJsonJson']);
-        $this->assertEquals($this->testsData['result4'], $this->testsData['diffYamlJson']);
-    }
+//    public function testGenDiffStylish()
+//    {
+//        $this->assertEquals($this->testsData['result2'], $this->testsData['diffJsonStylish']);
+//        $this->assertEquals($this->testsData['result2'], $this->testsData['diffYamlStylish']);
+//    }
+//
+//    public function testGenDiffPlain()
+//    {
+//        $this->assertEquals($this->testsData['result3'], $this->testsData['diffJsonPlain']);
+//        $this->assertEquals($this->testsData['result3'], $this->testsData['diffYamlPlain']);
+//    }
+//
+//    public function testGenDiffJson()
+//    {
+//        $this->assertEquals($this->testsData['result4'], $this->testsData['diffJsonJson']);
+//        $this->assertEquals($this->testsData['result4'], $this->testsData['diffYamlJson']);
+//    }
 }
