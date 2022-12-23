38c0fdec500a30281ad8369aa515c250b1241ff5
Apanasenko Andrey <aw_apanasenko@mail.ru>
2022-12-23 11:33:32 +0000

    deleted comments in DifferTest.php

--- tests/Differ/DifferTest.php
+++ tests/Differ/DifferTest.php
@@ -8,25 +8,6 @@ use function Differ\Differ\genDiff as genDiff;
 
 class DifferTest extends TestCase
 {
-//    private $testsData;
-
-//    public function setUp(): void
-//    {
-//        $this->testsData = [
-//            'result2' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result2'),
-//            'result3' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result3'),
-//            'result4' => file_get_contents(dirname(__DIR__, 1) . '/fixtures/result4.json'),
-//            'diffJson' => genDiff('file3.json', 'file4.json'),
-//            'diffYaml' => genDiff('file3.yml', 'file4.yaml'),
-//            'diffJsonStylish' => genDiff('file3.json', 'file4.json', 'stylish'),
-//            'diffYamlStylish' => genDiff('file3.yml', 'file4.yaml', 'stylish'),
-//            'diffJsonPlain' => genDiff('file3.json', 'file4.json', 'plain'),
-//            'diffYamlPlain' => genDiff('file3.yml', 'file4.yaml', 'plain'),
-//            'diffJsonJson' => genDiff('file3.json', 'file4.json', 'json'),
-//            'diffYamlJson' => genDiff('file3.yml', 'file4.yaml', 'json'),
-//        ];
-//    }
-
     /**
      * @dataProvider differProvider
      *
@@ -57,22 +38,4 @@ class DifferTest extends TestCase
             ['file3.yml', 'file4.yaml', 'result4.json', 'json'],
         ];
     }
-
-//    public function testGenDiffStylish()
-//    {
-//        $this->assertEquals($this->testsData['result2'], $this->testsData['diffJsonStylish']);
-//        $this->assertEquals($this->testsData['result2'], $this->testsData['diffYamlStylish']);
-//    }
-//
-//    public function testGenDiffPlain()
-//    {
-//        $this->assertEquals($this->testsData['result3'], $this->testsData['diffJsonPlain']);
-//        $this->assertEquals($this->testsData['result3'], $this->testsData['diffYamlPlain']);
-//    }
-//
-//    public function testGenDiffJson()
-//    {
-//        $this->assertEquals($this->testsData['result4'], $this->testsData['diffJsonJson']);
-//        $this->assertEquals($this->testsData['result4'], $this->testsData['diffYamlJson']);
-//    }
 }
